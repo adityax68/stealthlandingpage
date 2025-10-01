71cd6c56422b35034908be758a12713dc0d8dd1d
Aditya Maurya <adityamaurya@Adityas-MacBook-Air.local>
2025-10-02 00:27:03 +0530

    fixed api url error

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:8000',
-        changeOrigin: true,
-        secure: process.env.VITE_API_URL?.startsWith('https') || false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:8000',
+          changeOrigin: true,
+          secure: env.VITE_API_URL?.startsWith('https') || false,
+        }
       }
     }
   }
